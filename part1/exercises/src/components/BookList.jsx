5c9d92fb3951cd909176f8566e2d8b090cdc3d12
LifeIsFiction5 <jacob_barrett@icloud.com>
2024-08-19 18:04:09 -0500

    Saving where I was told to save

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I've read because I like to read";
+   let book1 = "https://m.media-amazon.com/images/I/51ymen1ByKL._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71vK0WVQ4rL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/71Tze+xZcgL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="A Short History of Nearly Everything" />
+         <img src={book2} alt="How to Win Friends and Influence People" />
+         <img src={book3} alt="The Obstacle is the Way" />
       </div>      
    );
 }
